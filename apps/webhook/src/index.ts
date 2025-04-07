05a10cad171816a9eb3ae00a4f32b8df39996382
TheManishDaksh <140195332+TheManishDaksh@users.noreply.github.com>
2025-04-08 00:08:36 +0530

    adding webhook

---
+++ apps/webhook/src/index.ts
@@ -0,0 +1,45 @@
+import  Express  from "express"
+import db from "@repo/db/client"
+const app = Express();
+app.use(Express.json());
+
+interface paymentInfo {
+    token : string
+    userId : string
+    amount : string
+}
+app.post("/hdfcbank", async(req, res )=>{
+    const paymentInformation : paymentInfo = {
+        token : req.body.token,
+        userId : req.body.id,
+        amount : req.body.amount
+    }
+    try{
+        await db.$transaction([
+            db.balance.updateMany({
+                where : {
+                    userId : Number(paymentInformation.userId)
+                },
+                data : {
+                    amount  : Number(paymentInformation.amount)
+                }
+            }),
+            db.balance.updateMany({
+                where : {
+                    token : paymentInformation.token    
+                },
+                data : {
+                    status : "Succeed"
+                }
+            })
+        ])
+        res.json({
+            message : 'captured'
+        })
+    }catch(error : any){
+        console.log(error);
+        res.status(403).json({
+            message : "There are problem while processing bank_webhook"
+        })
+    }
+})
